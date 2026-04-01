ef29838c279c528dbf082c6843176fc9206a53f7
TraaaaaaaaaaNKS <josediogogatao@gmail.com>
2026-04-01 18:04:58 -0300

    ip

---
+++ Aulas_js/auladevclub/DOM/ambiente.js
@@ -0,0 +1,8 @@
+let valorReal = document.querySelector("input");
+
+function converter() {
+    const ValorConvertido = valorReal.value / 5.20;
+
+    alert(`${valorReal} Reais está valendo %{ValorConvertido} Dólares!`);
+}
+
--- Aulas_js/auladevclub/funcoes.js
+++ Aulas_js/auladevclub/funcoes.js
@@ -1,8 +1,24 @@
-function soma(numero1, numero2){
-    const res = numero1 + numero2
-
-    return res
+function divida(receita, gastos){
+    if(receita > gastos) {
+        return "Não está endividado!"
+    } else {
+        return "Está endividado!"
+    }
 }
 
-let resultadoDaSoma = soma(30, 40)
-console.log(resultadoDaSoma)
+const maria = divida(5000, 7000)
+const joao = divida(10000, 8000)
+
+console.log(maria)
+console.log(joao)
+
+
+/* Arrow Function (Função flecha) */
+
+function abacate(){} //Forma clássica de função
+
+const digaMeuNome = () => {
+    console.log("Diogo")
+}
+
+DigaMeuNome2()
