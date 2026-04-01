cca71031de1eb8dafa4093d358ab9c1b847c4474
TraaaaaaaaaaNKS <josediogogatao@gmail.com>
2026-04-01 16:47:02 -0300

    update

---
+++ Aulas_js/auladevclub/aulaifeelse.js
@@ -0,0 +1,8 @@
+let cidade = "Arcoverde";
+let hab = 83.000;
+if(hab > 300.000) {
+    console.log(`${cidade} é considerada uma cidade grande!`)
+} else {
+    console.log(`${cidade} é uma cidade considerada Pequena!`)
+}
+
---
+++ Aulas_js/auladevclub/funcoes.js
@@ -0,0 +1,8 @@
+function soma(numero1, numero2){
+    const res = numero1 + numero2
+
+    return res
+}
+
+let resultadoDaSoma = soma(30, 40)
+console.log(resultadoDaSoma)
